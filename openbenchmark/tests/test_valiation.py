a1cd6bc2f5add55728e9ed34f36831d1cc581e29
Matthew W. Thompson <matt.thompson@vanderbilt.edu>
2020-04-15 17:02:28 -0500

    Add basic bond order santiy-checking function and test

--- openbenchmark/tests/test_valiation.py
+++ openbenchmark/tests/test_valiation.py
@@ -3,20 +3,26 @@ import pytest
 import qcportal as ptl
 from openforcefield.utils.toolkits import UndefinedStereochemistryError
 
-from openbenchmark.validation import run_through_openeye
+from openbenchmark.validation import run_through_openeye, sanity_check_bond_order
 
 
 client = ptl.FractalClient()
 
 ds = client.get_collection('OptimizationDataset', 'OpenFF Full Optimization Benchmark 1')
 
+good_smiles_in = 'CC(C)O-0'
+bad_smiles_in = 'CO/N=C/1\C[N@](C[C@H]1C[NH3+])c2c(cc3c(=O)c(cn(c3n2)C4CC4)C(=O)[O-])F-3'
 
-def test_run_through_openeye_good():
-    good_smiles_in = 'CC(C)O-0'
+
+def test_run_through_openeye():
     run_through_openeye(ds.get_record(good_smiles_in, specification='default'))
 
-def test_run_through_openeye_bad():
-    # TODO: Split this out into various tests capturing specific
-    bad_smiles_in = 'CO/N=C/1\C[N@](C[C@H]1C[NH3+])c2c(cc3c(=O)c(cn(c3n2)C4CC4)C(=O)[O-])F-3'
     with pytest.raises(UndefinedStereochemistryError):
         run_through_openeye(ds.get_record(bad_smiles_in, specification='default'))
+
+
+def test_bond_santity_checker():
+    sanity_check_bond_order(good_smiles_in, ds=ds)
+
+    with pytest.raises(AssertionError):
+        sanity_check_bond_order(bad_smiles_in, ds=ds)
--- openbenchmark/validation.py
+++ openbenchmark/validation.py
@@ -24,3 +24,36 @@ def run_through_openeye(record):
     oechem.OEAssignImplicitHydrogens(oemol)
     oechem.OEAssignFormalCharges(oemol)
     Molecule.from_openeye(oemol).to_rdkit()
+
+
+def sanity_check_bond_order(val, ds=None):
+    # TODO: get the smiles/cmiles in from the 'extras' in the record
+    entry = ds.get_entry(val)
+    smiles_in = entry.dict()['attributes']['canonical_isomeric_explicit_hydrogen_smiles']
+    mol_in = Molecule.from_smiles(smiles_in)
+    mol_in.assign_fractional_bond_orders(bond_order_model='am1-wiberg')
+    expected_bond_orders = [b.bond_order for b in mol_in.bonds]
+
+    record = ds.get_record(val, specification='default')
+
+    coords = record.get_final_molecule().dict()['geometry']
+    geometry = np.asarray(coords, dtype=float)*BOHR_2_ANGSTROM
+    symbols = record.get_final_molecule().dict()['symbols']
+
+    oemol = oechem.OEMol()
+
+    for s in symbols:
+        oemol.NewAtom(_symbols[s])
+
+    oemol.SetCoords(oechem.OEFloatArray(geometry.reshape(-1)))
+    oemol.SetDimension(3)
+    oechem.OEDetermineConnectivity(oemol)
+    oechem.OEDetermineConnectivity(oemol)
+    oechem.OEFindRingAtomsAndBonds(oemol)
+    oechem.OEAssignImplicitHydrogens(oemol)
+    oechem.OEPerceiveBondOrders(oemol)
+
+    found_bond_orders = [b.GetOrder() for b in oemol.GetBonds()]
+
+    for x, y in zip(expected_bond_orders, found_bond_orders):
+        assert x == y
