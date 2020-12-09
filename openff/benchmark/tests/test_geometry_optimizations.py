a50abbe750a5b541e61464628b3b13abde81ab55
David Dotson <dotsdl@gmail.com>
2020-12-08 21:06:35 -0700

    Added stubs for tests of geometry optimization components

---
+++ openff/benchmark/tests/test_geometry_optimizations.py
@@ -0,0 +1,23 @@
+"""Tests for geometry optimization library components.
+
+"""
+
+import pytest
+
+def test_submit_molecules():
+    pass
+
+def test_export_molecule_data():
+    pass
+
+def test_get_optimization_status():
+    pass
+
+def test_errorcycle_optimizations():
+    pass
+
+def test_optimization_from_server():
+    pass
+
+def test_execute_optimization_from_molecules():
+    pass
