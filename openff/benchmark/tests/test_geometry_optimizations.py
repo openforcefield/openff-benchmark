4a35103cae4c17ef9c552f9e4fc1232160e67be6
David Dotson <dotsdl@gmail.com>
2020-12-09 12:41:54 -0700

    Compute test work

--- openff/benchmark/tests/test_geometry_optimizations.py
+++ openff/benchmark/tests/test_geometry_optimizations.py
@@ -4,6 +4,9 @@
 
 import pytest
 
+from openff.benchmark.geometry_optimization import compute
+from openff.benchmark.utils.utils import get_data_file_path
+
 def test_submit_molecules():
     pass
 
@@ -19,5 +22,12 @@ def test_errorcycle_optimizations():
 def test_optimization_from_server():
     pass
 
-def test_execute_optimization_from_molecules():
-    pass
+def test_execute_optimization_from_molecules(tmpdir):
+    with tmpdir.as_cwd():
+
+        input_mols = [get_data_file_path('ethane.sdf')]
+
+        compute.execute_optimization_from_molecules(
+                input_mols, 
+                '3-export-compute',
+                '1:1')
