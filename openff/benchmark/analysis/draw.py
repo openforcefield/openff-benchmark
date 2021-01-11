b2560ecec5ec76e4522b8196703212aaa30f6e33
dfhahn <davidfriedrichhahn@gmail.com>
2021-01-11 21:28:05 +0100

    Add additional plots.

--- openff/benchmark/analysis/draw.py
+++ openff/benchmark/analysis/draw.py
@@ -16,7 +16,7 @@ import pandas as pd
 from scipy.interpolate import interpn
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 from plotly import graph_objects as go
 
 def draw_scatter(
@@ -205,9 +205,9 @@ def draw_ridgeplot(
         }
 
     # Initialize the FacetGrid object
-    my_cmap = "tab10"
-    sns.palplot(sns.color_palette(my_cmap))
-    colors = sns.color_palette(my_cmap)
+#    my_cmap = "tab10"
+#    sns.palplot(sns.color_palette(my_cmap))
+#    colors = sns.color_palette(my_cmap)
 
 
     fig = go.Figure()
@@ -355,7 +355,7 @@ def draw_density2d(
     nan_inds = np.isnan(x_data)
     x_data = x_data[~nan_inds]
     y_data = y_data[~nan_inds]
-    print(f"\nNumber of data points in FF scatter plot: {len(x_data)}")
+#    print(f"\nNumber of data points in FF scatter plot: {len(x_data)}")
 
     # compute histogram in 2d
     data, x_e, y_e = np.histogram2d(x_data, y_data, bins=bins)
@@ -389,13 +389,13 @@ def draw_density2d(
     idx = z.argsort()
     x, y, z = x_data[idx], y_data[idx], z[idx]
 
-    print(
-        f"{title} ranges of data in density plot:\n\t\tmin\t\tmax"
-        f"\nx\t{np.min(x):10.4f}\t{np.max(x):10.4f}"
-        f"\ny\t{np.min(y):10.4f}\t{np.max(y):10.4f}"
-        f"\nz\t{np.min(data):10.4f}\t{np.max(data):10.4f} (histogrammed)"
-        f"\nz'\t{np.nanmin(z):10.4f}\t{np.nanmax(z):10.4f} (interpolated)"
-    )
+    # print(
+    #     f"{title} ranges of data in density plot:\n\t\tmin\t\tmax"
+    #     f"\nx\t{np.min(x):10.4f}\t{np.max(x):10.4f}"
+    #     f"\ny\t{np.min(y):10.4f}\t{np.max(y):10.4f}"
+    #     f"\nz\t{np.min(data):10.4f}\t{np.max(data):10.4f} (histogrammed)"
+    #     f"\nz'\t{np.nanmin(z):10.4f}\t{np.nanmax(z):10.4f} (interpolated)"
+    # )
 
     # add dummy points of user-specified min/max z for uniform color scaling
     # similar to using vmin/vmax in pyplot pcolor
@@ -403,7 +403,7 @@ def draw_density2d(
     y = np.append(y, [0, 0])
     z = np.append(z, [z_range[0], z_range[1]])
 
-    print(f"z''\t{np.nanmin(z):10.4f}\t{np.nanmax(z):10.4f} (interp, bounded)")
+    # print(f"z''\t{np.nanmin(z):10.4f}\t{np.nanmax(z):10.4f} (interp, bounded)")
 
     # generate the plot
     plt.scatter(x, y, c=z, vmin=z_range[0], vmax=z_range[1], **plt_options)
@@ -416,7 +416,7 @@ def draw_density2d(
     colorbar_and_finish(size1, out_file)
 
 
-def plot_compare_ffs(results_dir):
+def plot_compare_ffs(results_dir, ref_method):
     results = {}
     for path in results_dir:
         if (os.path.isfile(path) and path.split('.')[-1].lower() == 'csv'):
@@ -429,6 +429,9 @@ def plot_compare_ffs(results_dir):
                     if (os.path.isfile(path) and path.split('.')[-1].lower() == 'csv'):
                         method = '.'.join(file.split('.')[:-1])
                         results[method] = pd.read_csv(path)
+
+    plot_violin_signed(results)
+    plot_mol_minima(results, ref_method)
 
     for method, result in results.items():
         # ddE vs RMSD scatter
@@ -547,7 +550,7 @@ def plot_compare_ffs(results_dir):
         'ddE (kcal/mol)',
         out_file=f'fig_ridge_dde.png',
         what_for="talk",
-        bw="kde",
+        bw="hist",
         same_subplot=True,
         sym_log=False,
         hist_range=(-15,15)
@@ -578,3 +581,275 @@ def plot_compare_ffs(results_dir):
 
 
 
+def plot_violin_signed(dataframes, what_for='talk'):
+    """
+    Generate violin plots of the mean signed errors
+    of force field energies with respect to QM energies.
+
+    Parameters
+    ----------
+    msds : 2D numpy array
+        Mean signed deviations for each method with reference to first method
+        msds[i][j] represents ith mol, jth method's MSE
+    ff_list : list
+        list of methods corresponding to energies in msds
+    what_for : string
+        dictates figure size, text size of axis labels, legend, etc.
+        "paper" or "talk"
+
+    """
+    
+#     # print stats on number of datapoints in each method
+#     temp = msds.T
+#     for l, m in zip(temp, ff_list):
+#         print(f"Total number of unique molecules: {l.shape}")
+#         print(f"Count of non-nan molecules for {m}: {np.count_nonzero(~np.isnan(l))}")
+
+#     # create dataframe from list of lists
+#     df = pd.DataFrame.from_records(msds, columns=ff_list)
+#     medians = df.median(axis=0)
+
+#     # reshape for grouped violin plots
+#     df = df.melt(value_vars=list(df.columns))
+#     df = df.dropna()
+    
+#     # set grid background style
+#     sns.set(style="whitegrid")
+
+#     if what_for == 'paper':
+# #        f, ax = plt.subplots(figsize=(2, 5))
+#         f, ax = plt.subplots(figsize=(8, 5))
+#         large_font = 10
+#         small_font = 8
+#         lw = 1
+#         med_pt = 5
+#         xrot = 45
+#         xha = 'right'
+#     elif what_for == 'talk':
+#         f, ax = plt.subplots(figsize=(10, 8))
+#         large_font = 16
+#         small_font = 14
+#         lw = 2
+#         med_pt = 10
+#         xrot = 0
+#         xha = 'center'
+
+#         # overlapping violins
+#         #lw=1.0
+#         #f, ax = plt.subplots(figsize=(4, 8))
+
+#     my_cmap = "tab10"
+#     colors = sns.color_palette(my_cmap)
+
+#     # show each distribution with both violins and points
+#     ax = sns.violinplot(x='variable', y='value', data=df, inner="box",
+#                         palette=colors, size=5, aspect=3, linewidth=lw)
+    fig = go.Figure()
+    for method, result in dataframes.items():
+        fig.add_trace(go.Violin(x0=method,
+                                y=result.loc[:,'dde'],
+                                hovertext=method,
+#                               legendgroup=idx, 
+    #                             alignmentgroup=idx,
+#                                offsetgroup=idx,
+#                                scalegroup=idx, 
+                                name=method,
+                                showlegend = False,
+#                                marker_color=clrs[i]
+                               )
+                     )
+
+    fig.write_image(f'violin.svg')
+    # replot the median point for larger marker, zorder to plot points on top
+    # xlocs = ax.get_xticks()
+    # for i, x in enumerate(xlocs):
+    #     plt.scatter(x, medians[i], marker='o', color='white', s=med_pt, zorder=100)
+
+    # # represent the y-data on log scale
+    # plt.yscale('symlog')
+
+    # # set alpha transparency
+    # plt.setp(ax.collections, alpha=0.8)
+
+    # # hide vertical plot boundaries
+    # sns.despine(left=True)
+
+    # # add labels and adjust font sizes
+    # ax.set_xlabel("")
+    # ax.set_ylabel("mean signed deviation (kcal/mol)", size=large_font)
+    # plt.xticks(fontsize=small_font, rotation=xrot, ha=xha)
+
+    # # settings for overlapping violins
+    # # plt.xticks([])
+    # # locs, labels = plt.yticks(fontsize=large_font)
+    # # plt.yticks(locs, [])
+    # # plt.xlim(-1, 1)
+    # # ax.set_ylabel("", size=large_font)
+
+    # # save and close figure
+    # plt.savefig('violin.svg', dpi=600, bbox_inches='tight')
+    # #plt.show()
+    # plt.clf()
+    # plt.close(plt.gcf())
+
+    # # reset plot parameters (white grid)
+    # sns.reset_orig()
+
+
+
+def plot_mol_rmses(mol_name, rmses, xticklabels, eff_nconfs, ref_nconfs, what_for='talk'):
+    """
+    Generate bar plot of RMSEs of conformer energies for this molecule of
+    all methods compared to reference method.
+
+    Number of conformers used to calculate the RMSE is also plotted
+    as a solid line. The number of possible conformers available
+    by the reference method is plotted as a dashed line.
+
+    Parameters
+    ----------
+    mol_name : string
+        title of the mol being plotted
+    rmses : list
+        rmses[i] is the RMSE of this mol of reference compared to ith method
+    xticklabels : list
+        list of methods of the same length as rmses list; should not include
+        reference method label
+    eff_nconfs : list
+        effective number of conformers with non-nan values;
+        same format and length as rmses
+    ref_nconfs : int
+        number of conformers in the reference method
+    what_for : string
+        dictates figure size, text size of axis labels, legend, etc.
+        "paper" or "talk"
+
+    """
+
+    # set figure-related labels
+    plttitle = f"RMSEs of relative energies for\nmolecule {mol_name}"
+    ylabel = "RMSE (kcal/mol)"
+    figname = f"barRMSE_{mol_name}.png"
+
+    # define x locations by integer number of methods
+    x_locs = list(range(len(xticklabels)))
+
+    if what_for == 'paper':
+        fig = plt.gcf()
+        fig.set_size_inches(4, 3)
+        large_font = 14
+        small_font = 10
+    elif what_for == 'talk':
+        large_font = 18
+        small_font = 16
+
+    # label figure; label xticks before plot for better spacing
+    plt.title(plttitle, fontsize=large_font)
+    plt.ylabel(ylabel, fontsize=large_font)
+    plt.xticks(x_locs, xticklabels, fontsize=small_font, rotation=-30, ha='left')
+    plt.yticks(fontsize=small_font)
+
+    # define custom colors here if desired
+    #colors = ['tab:blue']*len(xticklabels)
+    colors = ['tab:blue']*2 + ['tab:orange']*2 + ['tab:green']*2
+
+    # plot rmses as bars
+    plt.bar(x_locs, rmses, color=colors, align='center', label='RMSE')
+
+    # plot number of conformers as lines
+    ax2 = plt.twinx()
+    ax2.plot(x_locs, eff_nconfs, color='k', alpha=0.5,
+             label='actual num confs')
+    ax2.axhline(ref_nconfs, color='k', alpha=0.5, ls='--',
+                label='reference num confs')
+
+    # format line graph properties, then add plot legend
+    ax2.set_ylabel('Number of conformers', fontsize=large_font)
+    ax2.tick_params(axis='y', labelsize=small_font)
+    ax2.yaxis.set_ticks(np.arange(min(eff_nconfs)-1, ref_nconfs+2, 1))
+    plt.legend()
+
+    # save and close figure
+    plt.savefig(figname, bbox_inches='tight')
+    #plt.show()
+    plt.clf()
+    plt.close(plt.gcf())
+
+
+def plot_mol_minima(dataframes, ref_method, what_for='talk', selected=None):
+    """
+    Generate line plot of conformer energies of all methods (single molecule).
+
+    Parameters
+    ----------
+    mol_name : string
+        title of the mol being plotted
+    minimaE : list of lists
+        minimaE[i][j] represents ith method and jth conformer energy
+    legend : list
+        list of strings with all method names in same order as minimaE
+    what_for : string
+        dictates figure size, text size of axis labels, legend, etc.
+        "paper" or "talk"
+    selected : list
+        list of indices for methods to be plotted; e.g., [0], [0, 4]
+
+    """
+    if what_for == 'paper':
+        fig = plt.figure(figsize=(7.5, 3))
+        large_font = 12
+        small_font = 10
+        xaxis_font = 6
+        mark_size = 5
+    elif what_for == 'talk':
+        fig = plt.figure(figsize=(20, 8))
+        large_font = 18
+        small_font = 14
+        xaxis_font = 10
+        mark_size = 9
+
+    for mid in dataframes[ref_method].molecule_index.unique():    
+        ref_confs = dataframes[ref_method].loc[dataframes[ref_method].molecule_index==mid]
+        ref_nconfs = ref_confs.shape[0]
+
+        # set figure-related labels
+        mol_name = list(ref_confs['name'])[0]
+        plttitle = f"Relative Energies of {mol_name} Minima"
+        ylabel = "ddE (kcal/mol)"
+        figname = f"minimaE_{mol_name}.png"
+        xlabs = ref_confs['conformer_index']
+
+        # create figure
+        ax = fig.gca()
+        # label figure; label xticks before plotting for better spacing.
+        plt.title(plttitle, fontsize=large_font)
+        plt.ylabel(ylabel, fontsize=large_font)
+        plt.xlabel("conformer index", fontsize=large_font)
+        plt.xticks(list(range(ref_nconfs)), xlabs, fontsize=xaxis_font)
+        plt.yticks(fontsize=small_font)
+
+        # define line colors and markers
+        colors = mpl.cm.rainbow(np.linspace(0, 1, len(dataframes)))
+        markers = [
+            "x", "^", "8", "d", "o", "s", "*", "p", "v", "<", "D", "+", ">", "."
+        ] * 10
+        
+        # plot the data
+        for i, method in enumerate(dataframes):
+            # define x's from integer range with step 1
+            xi = list(range(ref_nconfs))
+            # get the relative energies of method
+            energies = dataframes[method].loc[dataframes[method].molecule_index==mid]['dde']
+            # plot values
+            plt.plot(xi, energies, color=colors[i], label=method,
+                     marker=markers[i], markersize=mark_size, alpha=0.6)
+
+        # add legend, set plot limits, add grid
+        plt.legend(bbox_to_anchor=(1, 1), loc=2, prop={'size': small_font})
+
+        plt.grid()
+
+        # save and close figure
+        plt.savefig(figname, bbox_inches='tight')
+        plt.clf()
+        
