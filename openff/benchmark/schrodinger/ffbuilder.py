ccd2a690246eea1cd1018d45295c6bd0ee57b5b8
dfhahn <davidfriedrichhahn@gmail.com>
2021-03-23 10:35:19 +0100

    Update style [schrodinger].

--- openff/benchmark/schrodinger/ffbuilder.py
+++ openff/benchmark/schrodinger/ffbuilder.py
@@ -12,14 +12,16 @@ import subprocess
 import warnings
 from collections import defaultdict
 
-# DO NOT USE; MESSES UP LOGGING 
+# DO NOT USE; MESSES UP LOGGING
 # consider overriding the loglevel of any particularly noisy loggers individual
-#logging.disable(logging.WARNING) 
+# logging.disable(logging.WARNING)
 
 from ..utils.io import mols_from_paths
 
 
-def ffbuilder(input_paths, schrodinger_path, host_settings, opls_dir, recursive, output_path):
+def ffbuilder(
+    input_paths, schrodinger_path, host_settings, opls_dir, recursive, output_path
+):
     """Submit SDF molecules from given directory to a Schrodinger ffbuilder OPLS3e parameterization
 
     Parameters
@@ -35,65 +37,72 @@ def ffbuilder(input_paths, schrodinger_path, host_settings, opls_dir, recursive,
     recursive : bool
         If True, recursively load SDFs from any directories given in `input_paths`.
     output_path: str
-        Path where output sdf files should be written to    
+        Path where output sdf files should be written to
     """
     # extract molecules from SDF inputs
     mols = mols_from_paths(input_paths, recursive=recursive)
-    
+
     os.makedirs(output_path, exist_ok=True)
-    os.makedirs(f'{output_path}', exist_ok=True)
-    
-    if os.path.exists(os.path.join(output_path, 'ffb_openff_benchmark.zip')):
-        warnings.warn(f'Schrodinger ffbuilder has been run already. '\
-                      f'Merging parameters and moving files to backup.')
+    os.makedirs(f"{output_path}", exist_ok=True)
+
+    if os.path.exists(os.path.join(output_path, "ffb_openff_benchmark.zip")):
+        warnings.warn(
+            f"Schrodinger ffbuilder has been run already. "
+            f"Merging parameters and moving files to backup."
+        )
         ffb_merge(schrodinger_path, opls_dir, output_path)
         ffb_backup(output_path)
 
-    with open(f'{output_path}/ffb_input.sdf', 'w') as file:
+    with open(f"{output_path}/ffb_input.sdf", "w") as file:
         for mol in mols:
-            if mol.name.endswith('00'):
-                mol.to_file(file, 'SDF')
+            if mol.name.endswith("00"):
+                mol.to_file(file, "SDF")
 
-    command = [os.path.join(schrodinger_path, 'ffbuilder'),
-               '-JOBNAME', 'ffb_openff_benchmark',
-               'ffb_input.sdf',
-               '-fit_advanced',
-               '-OPLSDIR', opls_dir,
-               '-HOST', host_settings,
-               '-TMPLAUNCHDIR'
+    command = [
+        os.path.join(schrodinger_path, "ffbuilder"),
+        "-JOBNAME",
+        "ffb_openff_benchmark",
+        "ffb_input.sdf",
+        "-fit_advanced",
+        "-OPLSDIR",
+        opls_dir,
+        "-HOST",
+        host_settings,
+        "-TMPLAUNCHDIR",
     ]
     subprocess.run(command, cwd=output_path)
 
 
-
 def ffb_merge(schrodinger_path, opls_dir, input_path):
-    if os.path.exists(os.path.join(input_path, 'ffb_openff_benchmark_oplsdir')):
-        command = [os.path.join(schrodinger_path, 'run'),
-                   '-FROM',
-                   'ffld',
-                   'merge_opls_data.py',
-                   os.path.join(input_path, 'ffb_openff_benchmark_oplsdir'),
-                   '-o',
-                   opls_dir
+    if os.path.exists(os.path.join(input_path, "ffb_openff_benchmark_oplsdir")):
+        command = [
+            os.path.join(schrodinger_path, "run"),
+            "-FROM",
+            "ffld",
+            "merge_opls_data.py",
+            os.path.join(input_path, "ffb_openff_benchmark_oplsdir"),
+            "-o",
+            opls_dir,
         ]
         subprocess.run(command)
 
 
-
 def ffb_backup(output_path):
-    os.makedirs(f'{output_path}', exist_ok=True)
-    backup_base = 'ffb_openff_benchmark.bk'
+    os.makedirs(f"{output_path}", exist_ok=True)
+    backup_base = "ffb_openff_benchmark.bk"
     backup_dir = backup_base
     num = 1
     while os.path.exists(os.path.join(output_path, backup_dir)):
-        backup_dir = f'{backup_base}.{num}'
+        backup_dir = f"{backup_base}.{num}"
         num += 1
-    backup_path = os.path.join(f'{output_path}', backup_dir)
+    backup_path = os.path.join(f"{output_path}", backup_dir)
     os.makedirs(backup_path)
-    for obj in ['ffb_openff_benchmark_oplsdir',
-                'ffb_input.sdf',
-                'ffb_openff_benchmark.log',
-                'ffb_openff_benchmark.zip']:
-        obj_path = os.path.join(f'{output_path}', obj)
+    for obj in [
+        "ffb_openff_benchmark_oplsdir",
+        "ffb_input.sdf",
+        "ffb_openff_benchmark.log",
+        "ffb_openff_benchmark.zip",
+    ]:
+        obj_path = os.path.join(f"{output_path}", obj)
         if os.path.exists(obj_path):
             shutil.move(obj_path, backup_path)
--- openff/benchmark/schrodinger/optimization.py
+++ openff/benchmark/schrodinger/optimization.py
@@ -12,32 +12,49 @@ import subprocess
 import warnings
 from collections import defaultdict
 
-# DO NOT USE; MESSES UP LOGGING 
+# DO NOT USE; MESSES UP LOGGING
 # consider overriding the loglevel of any particularly noisy loggers individual
-#logging.disable(logging.WARNING) 
+# logging.disable(logging.WARNING)
 
 from openforcefield.topology import Molecule
 from ..utils.io import mols_from_paths
 
+
 def sdf_to_mae(sdf_file, mae_file, schrodinger_path, append=True):
-    command = [os.path.join(schrodinger_path, 'utilities', 'sdconvert'),
-               '-isd', sdf_file,
-               '-omae', mae_file,
+    command = [
+        os.path.join(schrodinger_path, "utilities", "sdconvert"),
+        "-isd",
+        sdf_file,
+        "-omae",
+        mae_file,
     ]
     if append:
-        command.append('-a')
+        command.append("-a")
     subprocess.run(command)
+
 
 def mae_to_sdf(mae_file, sdf_file, schrodinger_path, append=True):
-    command = [os.path.join(schrodinger_path, 'utilities', 'sdconvert'),
-               '-imae', mae_file,
-               '-osd', sdf_file,
+    command = [
+        os.path.join(schrodinger_path, "utilities", "sdconvert"),
+        "-imae",
+        mae_file,
+        "-osd",
+        sdf_file,
     ]
     if append:
-        command.append('-a')
+        command.append("-a")
     subprocess.run(command)
 
-def optimization(input_paths, schrodinger_path, host_settings, opls_dir, recursive, output_path, delete_existing=False):
+
+def optimization(
+    input_paths,
+    schrodinger_path,
+    host_settings,
+    opls_dir,
+    recursive,
+    output_path,
+    delete_existing=False,
+):
     """Submit SDF molecules from given directory to a Schrodinger macromodel OPLS3e minimization
 
     Parameters
@@ -53,7 +70,7 @@ def optimization(input_paths, schrodinger_path, host_settings, opls_dir, recursi
     recursive : bool
         If True, recursively load SDFs from any directories given in `input_paths`.
     output_path: str
-        Path where output sdf files should be written to    
+        Path where output sdf files should be written to
     delete_existing : bool (False)
             If True, delete existing directory if present.
     """
@@ -64,59 +81,68 @@ def optimization(input_paths, schrodinger_path, host_settings, opls_dir, recursi
             shutil.rmtree(output_path)
             os.makedirs(output_path)
         else:
-            raise Exception(f'Output directory {output_path} already exists. '
-                            'Specify `delete_existing=True` to remove or select a different output directory.')
+            raise Exception(
+                f"Output directory {output_path} already exists. "
+                "Specify `delete_existing=True` to remove or select a different output directory."
+            )
 
-    mae_file = os.path.join(output_path, 'mmod_input.maegz')
-    sdf_file = os.path.join(output_path, 'mmod_input.sdf')
+    mae_file = os.path.join(output_path, "mmod_input.maegz")
+    sdf_file = os.path.join(output_path, "mmod_input.sdf")
 
     mols = mols_from_paths(input_paths, recursive=recursive)
-    with open(sdf_file, 'w') as file:
+    with open(sdf_file, "w") as file:
         for i, mol in enumerate(mols):
             if opls_dir is None:
-                mol.properties['method'] = 'opls3e_default'
-                mol.properties['basis'] = 'opls'
-                mol.properties['program'] = 'macromodel'
+                mol.properties["method"] = "opls3e_default"
+                mol.properties["basis"] = "opls"
+                mol.properties["program"] = "macromodel"
             else:
-                mol.properties['method'] = 'opls3e_custom'
-                mol.properties['basis'] = 'opls'
-                mol.properties['program'] = 'macromodel'
-            mol.to_file(file, 'SDF')
-            
-    sdf_to_mae(sdf_file, mae_file, schrodinger_path)
-    
-    com_file = os.path.join(output_path, 'mmod_openff_benchmark.com')
-    with open(com_file, 'w') as outfile:
-        outfile.write('mmod_input.maegz\n')
-        outfile.write('mmod_output.maegz\n')
-        outfile.write(' MMOD       0      1      0      0     0.0000     0.0000     0.0000     0.0000\n'\
-                      ' DEBG      55      0      0      0     0.0000     0.0000     0.0000     0.0000\n'\
-                      ' FFLD      16      1      0      0     1.0000     0.0000     0.0000     0.0000\n'\
-                      ' BDCO       0      0      0      0    41.5692 99999.0000     0.0000     0.0000\n'\
-                      ' CRMS       0      0      0      0     0.0000     0.5000     0.0000     0.0000\n'\
-                      ' BGIN       0      0      0      0     0.0000     0.0000     0.0000     0.0000\n'\
-                      ' READ       0      0      0      0     0.0000     0.0000     0.0000     0.0000\n'\
-                      ' CONV       1      0      0      0     5.0e-9     0.0000     0.0000     0.0000\n'\
-                      ' MINI      10      0   1500      0     0.0000     0.0000     0.0000     0.0000\n'\
-                      ' END        0      0      0      0     0.0000     0.0000     0.0000     0.0000\n')
+                mol.properties["method"] = "opls3e_custom"
+                mol.properties["basis"] = "opls"
+                mol.properties["program"] = "macromodel"
+            mol.to_file(file, "SDF")
 
-        
-    command = [os.path.join(schrodinger_path, 'macromodel'),
-               'mmod_openff_benchmark.com'
-           ]
+    sdf_to_mae(sdf_file, mae_file, schrodinger_path)
+
+    com_file = os.path.join(output_path, "mmod_openff_benchmark.com")
+    with open(com_file, "w") as outfile:
+        outfile.write("mmod_input.maegz\n")
+        outfile.write("mmod_output.maegz\n")
+        outfile.write(
+            " MMOD       0      1      0      0     0.0000     0.0000     0.0000     0.0000\n"
+            " DEBG      55      0      0      0     0.0000     0.0000     0.0000     0.0000\n"
+            " FFLD      16      1      0      0     1.0000     0.0000     0.0000     0.0000\n"
+            " BDCO       0      0      0      0    41.5692 99999.0000     0.0000     0.0000\n"
+            " CRMS       0      0      0      0     0.0000     0.5000     0.0000     0.0000\n"
+            " BGIN       0      0      0      0     0.0000     0.0000     0.0000     0.0000\n"
+            " READ       0      0      0      0     0.0000     0.0000     0.0000     0.0000\n"
+            " CONV       1      0      0      0     5.0e-9     0.0000     0.0000     0.0000\n"
+            " MINI      10      0   1500      0     0.0000     0.0000     0.0000     0.0000\n"
+            " END        0      0      0      0     0.0000     0.0000     0.0000     0.0000\n"
+        )
+
+    command = [
+        os.path.join(schrodinger_path, "macromodel"),
+        "mmod_openff_benchmark.com",
+    ]
     if host_settings is not None:
-        command.append('-HOST')
-        command.append(f'{host_settings}')
+        command.append("-HOST")
+        command.append(f"{host_settings}")
     if opls_dir is not None:
-        command.append('-OPLSDIR')
+        command.append("-OPLSDIR")
         command.append(os.path.abspath(opls_dir))
     subprocess.run(command, cwd=os.path.join(output_path))
 
 
-
-def postprocess(input_paths, schrodinger_path, output_path, delete_existing=False, keep_existing=True):
+def postprocess(
+    input_paths,
+    schrodinger_path,
+    output_path,
+    delete_existing=False,
+    keep_existing=True,
+):
     """Postprocess output from Schrodinger macromodel OPLS3e minimization.
-    
+
     Parameters
     ----------
     input_paths : str or list of str
@@ -124,7 +150,7 @@ def postprocess(input_paths, schrodinger_path, output_path, delete_existing=Fals
     schrodinger_path : str
         Path to Schrodinger executables
     output_path: str
-        Path where output sdf files should be written to    
+        Path where output sdf files should be written to
     delete_existing : bool (False)
             If True, delete existing directory if present.
     keep_existing : bool (True)
@@ -134,23 +160,27 @@ def postprocess(input_paths, schrodinger_path, output_path, delete_existing=Fals
 
     mols = []
     for path in input_paths:
-        if (os.path.isfile(path) and (path.split('.')[-1].lower() == 'maegz' or path.split('.')[-1].lower() == 'mae')):
-            sdf_file = '.'.join(path.split('.')[:-1]+['sdf'])
+        if os.path.isfile(path) and (
+            path.split(".")[-1].lower() == "maegz"
+            or path.split(".")[-1].lower() == "mae"
+        ):
+            sdf_file = ".".join(path.split(".")[:-1] + ["sdf"])
             mae_to_sdf(path, sdf_file, schrodinger_path, append=False)
-            tmp_mols = Molecule.from_file(sdf_file, 'SDF')
+            tmp_mols = Molecule.from_file(sdf_file, "SDF")
             if type(tmp_mols) == Molecule:
                 mols.append(tmp_mols)
             else:
                 mols += tmp_mols
-    
 
     methods = set()
     for mol in mols:
-        mol.properties.pop('initial_energy')
-        mol.properties['final_energy'] = f'{mol.properties["r_mmod_Potential_Energy-OPLS3e"]} kJ / mole'
-        mol.properties['molecule_index'] = f"{mol.properties['molecule_index']:05d}"
-        mol.properties['conformer_index'] = f"{mol.properties['conformer_index']:02d}"
-        methods.add(mol.properties['method'])
+        mol.properties.pop("initial_energy")
+        mol.properties[
+            "final_energy"
+        ] = f'{mol.properties["r_mmod_Potential_Energy-OPLS3e"]} kJ / mole'
+        mol.properties["molecule_index"] = f"{mol.properties['molecule_index']:05d}"
+        mol.properties["conformer_index"] = f"{mol.properties['conformer_index']:02d}"
+        methods.add(mol.properties["method"])
 
     os.makedirs(output_path, exist_ok=True)
     for method in methods:
@@ -161,17 +191,20 @@ def postprocess(input_paths, schrodinger_path, output_path, delete_existing=Fals
             if delete_existing:
                 shutil.rmtree(output_directory)
                 os.makedirs(output_directory)
-            elif keep_existing: 
-                warnings.warn(f"Output directory {output_directory} exists and will be used for export; existing data files will not be replaced."
-                              "Specify `delete_existing=True` to replace.")
+            elif keep_existing:
+                warnings.warn(
+                    f"Output directory {output_directory} exists and will be used for export; existing data files will not be replaced."
+                    "Specify `delete_existing=True` to replace."
+                )
                 pass
             else:
-                raise Exception(f'Output directory {output_directory} already exists. '
-                                'Specify `delete_existing=True` to remove or specify another output directory.')
+                raise Exception(
+                    f"Output directory {output_directory} already exists. "
+                    "Specify `delete_existing=True` to remove or specify another output directory."
+                )
     for mol in mols:
-        outfile = os.path.join(output_path, mol.properties['method'], f'{mol.name}.sdf')
+        outfile = os.path.join(output_path, mol.properties["method"], f"{mol.name}.sdf")
         if (not delete_existing) and os.path.exists(outfile):
             print(f"... '{mol.name}' : skipping SDF exists")
             continue
-        mol.to_file(outfile, 'SDF')
-
+        mol.to_file(outfile, "SDF")
