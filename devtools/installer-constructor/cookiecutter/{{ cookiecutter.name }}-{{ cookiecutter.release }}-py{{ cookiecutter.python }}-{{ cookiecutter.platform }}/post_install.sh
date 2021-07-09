c79443361fd9c29b644b8b7e5c153c02b1022353
David Dotson <dotsdl@gmail.com>
2021-07-08 18:50:24 -0700

    Added conda activation to post-install on constructor installer

--- devtools/installer-constructor/cookiecutter/{{ cookiecutter.name }}-{{ cookiecutter.release }}-py{{ cookiecutter.python }}-{{ cookiecutter.platform }}/post_install.sh
+++ devtools/installer-constructor/cookiecutter/{{ cookiecutter.name }}-{{ cookiecutter.release }}-py{{ cookiecutter.python }}-{{ cookiecutter.platform }}/post_install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+conda activate /home/runner/constructor_install
+
 # Pip install the openff-forcefields package to pull in the sage release candidate
 pip install git+https://github.com/openforcefield/openff-forcefields.git@2.0.0-rc.1
 
